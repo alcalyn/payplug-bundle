39bd111b44df8d364970854c682dbb2a74829bf7
Julien Maulny <j.maulny.altimate@neopost.com>
2014-08-26 10:12:44 +0200

    refactor Entity to Model, comments

--- Services/PayplugService.php
+++ Services/PayplugService.php
@@ -2,7 +2,8 @@
 
 namespace Alcalyn\PayplugBundle\Services;
 
-use Alcalyn\PayplugBundle\Entity\Payment;
+use Symfony\Component\Routing\Router;
+use Alcalyn\PayplugBundle\Model\Payment;
 
 class PayplugService
 {
@@ -21,13 +22,21 @@ class PayplugService
     private $privateKey;
     
     /**
+     * IPN url
+     * 
+     * @var string
+     */
+    private $ipnUrl;
+    
+    /**
      * @param string $baseUrl Payplug url from account configuration
      * @param string $privateKey Your private key from account configuration
      */
-    public function __construct($baseUrl, $privateKey)
+    public function __construct($baseUrl, $privateKey, Router $router)
     {
         $this->baseUrl = $baseUrl;
         $this->privateKey = $privateKey;
+        $this->ipnUrl = $router->generate('payplug_ipn');
     }
     
     /**
@@ -39,23 +48,40 @@ class PayplugService
      */
     public function generateUrl(Payment $payment)
     {
-        $params = array(
-            'amount'        => $payment->getAmount(),
-            'currency'      => $payment->getCurrency(),
-            'ipn_url'       => $payment->getIpnUrl(),
-            'email'         => $payment->getEmail(),
-            'first_name'    => $payment->getFirstName(),
-            'last_name'     => $payment->getLastName(),
-        );
-        
+        // Create data parameter
+        $params = $this->convertPaymentToArray($payment);
         $url_params = http_build_query($params);
         $data = urlencode(base64_encode($url_params));
         
+        // Create signature parameter
         $signature = null;
         $privatekey = openssl_pkey_get_private($this->privateKey);
         openssl_sign($url_params, $signature, $privatekey, OPENSSL_ALGO_SHA1);
         $signatureBase64 = urlencode(base64_encode($signature));
         
         return $this->baseUrl . '?data=' . $data . '&sign=' . $signatureBase64;
+    }
+    
+    /**
+     * @param \Alcalyn\PayplugBundle\Model\Payment $payment
+     * 
+     * @return array
+     */
+    private function convertPaymentToArray(Payment $payment)
+    {
+        return array(
+            'amount'        => $payment->getAmount(),
+            'currency'      => $payment->getCurrency(),
+            'ipn_url'       => $payment->getIpnUrl() ?: $this->ipnUrl,
+            'return_url'    => $payment->getReturnUrl(),
+            'cancel_url'    => $payment->getCancelUrl(),
+            'email'         => $payment->getEmail(),
+            'first_name'    => $payment->getFirstName(),
+            'last_name'     => $payment->getLastName(),
+            'customer'      => $payment->getCustomer(),
+            'order'         => $payment->getOrder(),
+            'custom_data'   => $payment->getCustomData(),
+            'origin'        => $payment->getOrigin(),
+        );
     }
 }
