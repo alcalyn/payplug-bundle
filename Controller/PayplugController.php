e2b25dcff7a41fc232fe2b7ab16aa134c78dd9f8
Alcalyn <doubjulien@hotmail.fr>
2014-09-14 08:13:28 +0200

    add missing use statement

--- Controller/PayplugController.php
+++ Controller/PayplugController.php
@@ -4,6 +4,7 @@ namespace Alcalyn\PayplugBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Alcalyn\PayplugBundle\Event\PayplugIPNEvent;
 use Alcalyn\PayplugBundle\Event\PayplugMalformedIPNEvent;
