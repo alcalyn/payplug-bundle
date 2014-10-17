6a8fc959c9c7e905a76f8118584d730c075dca6f
Alcalyn <doubjulien@hotmail.fr>
2014-10-17 19:25:21 +0200

    doc, security flaw fix

--- DependencyInjection/Compiler/RegisterMappingsPass.php
+++ DependencyInjection/Compiler/RegisterMappingsPass.php
@@ -9,12 +9,38 @@ use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
 
 class RegisterMappingsPass implements CompilerPassInterface
 {
+    /**
+     * @var Definition|Reference
+     */
     private $driver;
+    
+    /**
+     * @var string
+     */
     private $driverPattern;
+    
+    /**
+     * @var array
+     */
     private $namespaces;
+    
+    /**
+     * @var array
+     */
     private $managerParameters;
+    
+    /**
+     * @var boolean
+     */
     private $enabledParameter;
 
+    /**
+     * @param Definition|Reference $driver
+     * @param string $driverPattern
+     * @param array $namespaces
+     * @param array $managerParameters
+     * @param boolean $enabledParameter
+     */
     public function __construct($driver, $driverPattern, $namespaces, $managerParameters, $enabledParameter = false)
     {
         $this->driver = $driver;
@@ -24,6 +50,9 @@ class RegisterMappingsPass implements CompilerPassInterface
         $this->enabledParameter = $enabledParameter;
     }
 
+    /**
+     * @param ContainerBuilder $container
+     */
     public function process(ContainerBuilder $container)
     {
         if ($this->enabledParameter && !$container->hasParameter($this->enabledParameter)) {
@@ -37,6 +66,13 @@ class RegisterMappingsPass implements CompilerPassInterface
         }
     }
 
+    /**
+     * @param ContainerBuilder $container
+     * 
+     * @return string
+     * 
+     * @throws ParameterNotFoundException
+     */
     protected function getChainDriverServiceName(ContainerBuilder $container)
     {
         foreach ($this->managerParameters as $param) {
@@ -51,6 +87,11 @@ class RegisterMappingsPass implements CompilerPassInterface
         throw new ParameterNotFoundException('None of the managerParameters resulted in a valid name');
     }
 
+    /**
+     * @param array $mappings
+     * 
+     * @return \Alcalyn\PayplugBundle\DependencyInjection\Compiler\RegisterMappingsPass
+     */
     public static function createOrmMappingDriver(array $mappings)
     {
         $arguments = array($mappings, '.orm.yml');
--- Exceptions/PayplugException.php
+++ Exceptions/PayplugException.php
@@ -4,6 +4,9 @@ namespace Alcalyn\PayplugBundle\Exceptions;
 
 class PayplugException extends \Exception
 {
+    /**
+     * @param string $message
+     */
     public function __construct($message)
     {
         parent::__construct($message, null, null);
